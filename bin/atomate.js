2d09773b34f2184e794d79ec58c6a2891b3abaa6
Brennan Moore <brennanmoore@gmail.com>
2011-06-05 19:06:12 -0400

    list of notes is now appending to le dom

--- bin/atomate.js
+++ bin/atomate.js
@@ -7,10 +7,21 @@
 Atomate = {
     initialize: function(params) {
         this.tracking.initialize(params);
-
+        this.notesList = jQuery("#notes");
+        this.addRedactedNotes(params.redactedNotes.slice(0, 100));
+    },
+    addRedactedNotes: function(notes) {
+        var this_ = this;
+        jQuery.fn.append.apply(this.notesList, notes.map(function(n){ return this_.getItemHtml(n); }));
+        //this.notesList.html(notes.map(function(n){ return this_.getItemHtml(n); }).join(''));
     },
 
+    getItemHtml: function(item) {
+        return "<li class=\"" +  item.category.toLowerCase() + "\">"
+            + item.contents + "</li>";
+            
 
+    }
 
 
 
