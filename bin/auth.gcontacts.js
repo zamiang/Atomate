8b3661d9fda5ed095436a66c6bd4bd164cf2e516
Brennan Moore <brennanmoore@gmail.com>
2011-06-26 15:58:26 -0400

    indenting google contacts file

--- bin/auth.gcontacts.js
+++ bin/auth.gcontacts.js
@@ -8,12 +8,12 @@ Atomate.auth.gcontacts = {
     parent: Atomate.auth,
     feedUri: 'https://www.google.com/m8/feeds/contacts/default/full',
     initialize: function() {
-	try {
-	    showMessage('logged in and about to start saving from Google contacts');
-	    this.getMyContacts();
-	} catch (x) {
-	    showMessage(x);
-	}
+	    try {
+	        showMessage('logged in and about to start saving from Google contacts');
+	        this.getMyContacts();
+	    } catch (x) {
+	        showMessage(x);
+	    }
     },
     
     getMyContacts: function() {
@@ -21,50 +21,50 @@ Atomate.auth.gcontacts = {
         
         query.setMaxResults(500);
 
-	/*
-	  Useful!
-	  // Set up the query to retrieve all contacts that has been modifed since today
-	  var today = new Date();
-	  var updatedMin = new google.gdata.DateTime(today, true);
-	  query.setUpdatedMin(updatedMin);
-	  // Sort result set with descending order
-	  query.setSortOrder('descending');
-	*/
+	    /*
+	     Useful!
+	     // Set up the query to retrieve all contacts that has been modifed since today
+	     var today = new Date();
+	     var updatedMin = new google.gdata.DateTime(today, true);
+	     query.setUpdatedMin(updatedMin);
+	     // Sort result set with descending order
+	     query.setSortOrder('descending');
+	     */
         ContactsService.getContactFeed(query, this.callback, handleError);
     },
     
     callback: function(result) {
-	showMessage('got a contacts callback');
+	    showMessage('got a contacts callback');
 
-	var now = new Date().valueOf();
-	var parent = Atomate.auth;
+	    var now = new Date().valueOf();
+	    var parent = Atomate.auth;
         var entries = result.feed.entry;
 
-	var contacts = entries.map(function(entry){
-		var start = entry.getUpdated() ? new Date(entry.getUpdated().getValue().getDate().format()).valueOf() : now;
-		var emailAddresses = entry.getEmailAddresses().map(function(e){ return e.getAddress(); });
-		var name = entry.getTitle().getText().split(' ');
-		var photoUrl = entry.getContactPhotoLink() ? entry.getContactPhotoLink().href : undefined; // useless :(
-		var birthday = entry.getBirthday() ? entry.getBirthday().getWhen() : undefined;
-		var websites = entry.getWebsites().map(function(w){ return w.href; });
-		var contact = {
-		    'type':'schemas.Person',
-		    'id': entry.id.getValue(),
-		    'first name': name[0],
-		    'last name': name[name.length - 1],
-		    'nickname' :entry.getNickname,
-		    'email addresses': emailAddresses,
-		    'photo url': photoUrl,
-		    'gender': entry.getGender(),
-		    'url': entry.id.getValue(),
-		    'websites': websites,
-		    'priority':entry.getPriority(),
-		    'last updated': parent.makeSpecificDateTime(start),
-		    'birthday': birthday
-		};
-		parent.saveItem(contact);
-		return contact;
-	    });
-	showMessage('just saved ' + contacts.length + ' contacts from Google Contacts');
+	    var contacts = entries.map(function(entry){
+		                               var start = entry.getUpdated() ? new Date(entry.getUpdated().getValue().getDate().format()).valueOf() : now;
+		                               var emailAddresses = entry.getEmailAddresses().map(function(e){ return e.getAddress(); });
+		                               var name = entry.getTitle().getText().split(' ');
+		                               var photoUrl = entry.getContactPhotoLink() ? entry.getContactPhotoLink().href : undefined; // useless :(
+		                               var birthday = entry.getBirthday() ? entry.getBirthday().getWhen() : undefined;
+		                               var websites = entry.getWebsites().map(function(w){ return w.href; });
+		                               var contact = {
+		                                   'type':'schemas.Person',
+		                                   'id': entry.id.getValue(),
+		                                   'first name': name[0],
+		                                   'last name': name[name.length - 1],
+		                                   'nickname' :entry.getNickname,
+		                                   'email addresses': emailAddresses,
+		                                   'photo url': photoUrl,
+		                                   'gender': entry.getGender(),
+		                                   'url': entry.id.getValue(),
+		                                   'websites': websites,
+		                                   'priority':entry.getPriority(),
+		                                   'last updated': parent.makeSpecificDateTime(start),
+		                                   'birthday': birthday
+		                               };
+		                               parent.saveItem(contact);
+		                               return contact;
+	                               });
+	    showMessage('just saved ' + contacts.length + ' contacts from Google Contacts');
     }
 };
