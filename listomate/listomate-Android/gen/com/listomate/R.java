7318d522fd85a66c315a02864d8b92b02c5b4249
Brennan Moore <brennanmoore@gmail.com>
2011-09-04 23:38:42 -0400

    working on note editing

--- listomate/listomate-Android/gen/com/listomate/R.java
+++ listomate/listomate-Android/gen/com/listomate/R.java
@@ -44,40 +44,37 @@ public final class R {
         public static final int btn_bg_pressed=0x7f020001;
         public static final int btn_bg_selected=0x7f020002;
         public static final int gradient=0x7f020003;
-        public static final int ic_launcher_default=0x7f020004;
-        public static final int ic_launcher_pressed=0x7f020005;
-        public static final int ic_launcher_selected=0x7f020006;
-        public static final int ic_list_sync_anim=0x7f020007;
-        public static final int ic_list_sync_anim0=0x7f020008;
-        public static final int ic_list_sync_anim1=0x7f020009;
-        public static final int ic_list_sync_anim2=0x7f02000a;
-        public static final int ic_list_sync_anim3=0x7f02000b;
-        public static final int ic_mailboxes_accounts=0x7f02000c;
-        public static final int ic_menu_account_list=0x7f02000d;
-        public static final int ic_menu_compose=0x7f02000e;
-        public static final int ic_menu_delete=0x7f02000f;
-        public static final int ic_menu_edit=0x7f020010;
-        public static final int ic_menu_preferences=0x7f020011;
-        public static final int ic_menu_refresh=0x7f020012;
-        public static final int ic_menu_revert=0x7f020013;
-        public static final int ic_title_add=0x7f020014;
-        public static final int ic_title_add_alt=0x7f020015;
-        public static final int ic_title_add_default=0x7f020016;
-        public static final int ic_title_add_light=0x7f020017;
-        public static final int ic_title_icon=0x7f020018;
-        public static final int ic_title_sync_anim=0x7f020019;
-        public static final int ic_title_sync_anim0=0x7f02001a;
-        public static final int ic_title_sync_anim1=0x7f02001b;
-        public static final int ic_title_sync_anim2=0x7f02001c;
-        public static final int ic_title_sync_anim3=0x7f02001d;
-        public static final int icon=0x7f02001e;
-        public static final int status_icon=0x7f02001f;
-        public static final int tab_bg_selected=0x7f020020;
-        public static final int tab_bg_selector=0x7f020021;
-        public static final int tab_bg_unselected=0x7f020022;
-        public static final int tab_divider=0x7f020023;
-        public static final int tab_text_selector=0x7f020024;
-        public static final int title_button=0x7f020025;
+        public static final int ic_list_sync_anim=0x7f020004;
+        public static final int ic_list_sync_anim0=0x7f020005;
+        public static final int ic_list_sync_anim1=0x7f020006;
+        public static final int ic_list_sync_anim2=0x7f020007;
+        public static final int ic_list_sync_anim3=0x7f020008;
+        public static final int ic_mailboxes_accounts=0x7f020009;
+        public static final int ic_menu_account_list=0x7f02000a;
+        public static final int ic_menu_compose=0x7f02000b;
+        public static final int ic_menu_delete=0x7f02000c;
+        public static final int ic_menu_edit=0x7f02000d;
+        public static final int ic_menu_preferences=0x7f02000e;
+        public static final int ic_menu_refresh=0x7f02000f;
+        public static final int ic_menu_revert=0x7f020010;
+        public static final int ic_title_add=0x7f020011;
+        public static final int ic_title_add_alt=0x7f020012;
+        public static final int ic_title_add_default=0x7f020013;
+        public static final int ic_title_add_light=0x7f020014;
+        public static final int ic_title_search_default=0x7f020015;
+        public static final int ic_title_sync_anim=0x7f020016;
+        public static final int ic_title_sync_anim0=0x7f020017;
+        public static final int ic_title_sync_anim1=0x7f020018;
+        public static final int ic_title_sync_anim2=0x7f020019;
+        public static final int ic_title_sync_anim3=0x7f02001a;
+        public static final int icon=0x7f02001b;
+        public static final int status_icon=0x7f02001c;
+        public static final int tab_bg_selected=0x7f02001d;
+        public static final int tab_bg_selector=0x7f02001e;
+        public static final int tab_bg_unselected=0x7f02001f;
+        public static final int tab_divider=0x7f020020;
+        public static final int tab_text_selector=0x7f020021;
+        public static final int title_button=0x7f020022;
     }
     public static final class id {
         public static final int add_location_button=0x7f0b0006;
@@ -85,7 +82,6 @@ public final class R {
         public static final int add_reminder_button=0x7f0b0004;
         public static final int connect=0x7f0b000d;
         public static final int connect_text=0x7f0b0008;
-        public static final int dateText=0x7f0b001a;
         public static final int detailsText=0x7f0b0002;
         public static final int disconnect=0x7f0b0010;
         public static final int disconnect_text=0x7f0b000f;
@@ -93,34 +89,33 @@ public final class R {
         public static final int exit=0x7f0b000e;
         public static final int footer=0x7f0b000c;
         public static final int header=0x7f0b0007;
-        public static final int imageView1=0x7f0b0013;
+        public static final int imageView1=0x7f0b0011;
         public static final int keyboardButtons=0x7f0b0003;
-        public static final int linearLayout1=0x7f0b0019;
-        public static final int list=0x7f0b0014;
+        public static final int linearLayout1=0x7f0b0012;
+        public static final int list=0x7f0b0016;
         public static final int saveButton=0x7f0b0001;
         public static final int select_account=0x7f0b000b;
         public static final int select_text=0x7f0b0009;
-        public static final int tabsLayout=0x7f0b0017;
-        public static final int tabsText=0x7f0b0018;
-        public static final int taskDate=0x7f0b0012;
-        public static final int taskTitle=0x7f0b0011;
-        public static final int titleText=0x7f0b001b;
+        public static final int tabsLayout=0x7f0b0018;
+        public static final int tabsText=0x7f0b0019;
+        public static final int taskDate=0x7f0b0015;
+        public static final int taskTitle=0x7f0b0014;
+        public static final int titleText=0x7f0b0013;
         public static final int title_refresh_progress=0x7f0b0000;
-        public static final int txt_appdata=0x7f0b0016;
-        public static final int txt_query=0x7f0b0015;
+        public static final int txt_query=0x7f0b0017;
     }
     public static final class layout {
         public static final int account=0x7f030000;
         public static final int addnote=0x7f030001;
         public static final int connect=0x7f030002;
         public static final int disconnect=0x7f030003;
-        public static final int listitem=0x7f030004;
-        public static final int listitemlayout=0x7f030005;
-        public static final int main=0x7f030006;
-        public static final int notelist=0x7f030007;
-        public static final int search=0x7f030008;
-        public static final int tabs_bg=0x7f030009;
-        public static final int viewtask=0x7f03000a;
+        public static final int editnote=0x7f030004;
+        public static final int listitem=0x7f030005;
+        public static final int listitemlayout=0x7f030006;
+        public static final int main=0x7f030007;
+        public static final int notelist=0x7f030008;
+        public static final int search=0x7f030009;
+        public static final int tabs_bg=0x7f03000a;
     }
     public static final class menu {
         public static final int main_menu=0x7f0a0000;
@@ -128,95 +123,97 @@ public final class R {
     public static final class string {
         /**  Menu item text [CHAR LIMIT=NONE] 
          */
-        public static final int accounts=0x7f08001b;
+        public static final int accounts=0x7f08001d;
         /**  Dialog button [CHAR LIMIT=NONE] 
          */
-        public static final int add_account=0x7f08001c;
-        public static final int add_note_add_location=0x7f080007;
-        public static final int add_note_add_person=0x7f080009;
-        public static final int add_note_add_reminder=0x7f080008;
-        public static final int add_note_hint=0x7f080006;
-        public static final int add_note_save=0x7f080005;
-        public static final int add_note_title=0x7f080004;
-        public static final int app_description=0x7f08001e;
-        public static final int app_name=0x7f08001d;
-        public static final int app_name_setup=0x7f080014;
+        public static final int add_account=0x7f08001e;
+        public static final int add_note_add_location=0x7f080009;
+        public static final int add_note_add_person=0x7f08000b;
+        public static final int add_note_add_reminder=0x7f08000a;
+        public static final int add_note_hint=0x7f080008;
+        public static final int add_note_save=0x7f080007;
+        public static final int add_note_title=0x7f080006;
+        public static final int app_description=0x7f080020;
+        public static final int app_name=0x7f08001f;
+        public static final int app_name_setup=0x7f080016;
         /**  Dialog title text [CHAR LIMIT=NONE] 
          */
-        public static final int attention=0x7f08001f;
-        public static final int cancel=0x7f08000c;
+        public static final int attention=0x7f080021;
+        public static final int cancel=0x7f08000e;
         /**  Button text [CHAR LIMIT=NONE] 
          */
-        public static final int connect=0x7f080020;
+        public static final int connect=0x7f080022;
         /**  Message text for "Connect" screen [CHAR LIMIT=NONE] 
          */
-        public static final int connect_text=0x7f080021;
+        public static final int connect_text=0x7f080023;
         /**  Header text for "Connect" screen [CHAR LIMIT=NONE] 
          */
-        public static final int connect_to_the_cloud=0x7f080022;
+        public static final int connect_to_the_cloud=0x7f080024;
         /**  Header text for "Connected" screen [CHAR LIMIT=NONE] 
          */
-        public static final int connected_to_the_cloud=0x7f080023;
+        public static final int connected_to_the_cloud=0x7f080025;
         /**  Message text [CHAR LIMIT=NONE] 
          */
-        public static final int contacting_server=0x7f080024;
-        public static final int contacts=0x7f080011;
-        public static final int dateButton=0x7f08000b;
+        public static final int contacting_server=0x7f080026;
+        public static final int contacts=0x7f080013;
+        public static final int dateButton=0x7f08000d;
         public static final int description_add=0x7f080001;
         public static final int description_logo=0x7f080000;
         public static final int description_search=0x7f080002;
         /**  Button text [CHAR LIMIT=NONE] 
          */
-        public static final int disconnect=0x7f080025;
+        public static final int disconnect=0x7f080027;
         /**  Informational text for "Connected" screen with placeholder for account 
 		name [CHAR LIMIT=NONE] 
          */
-        public static final int disconnect_text=0x7f080026;
+        public static final int disconnect_text=0x7f080028;
         /**  Footer text for "Connect" screen [CHAR LIMIT=NONE] 
          */
-        public static final int do_you_want_to_connect=0x7f080027;
+        public static final int do_you_want_to_connect=0x7f080029;
         /**  Do you want to disconnect textt [CHAR LIMIT=NONE] 
          */
-        public static final int do_you_want_to_disconnect=0x7f08002d;
-        public static final int events=0x7f080010;
-        public static final int exit=0x7f080030;
+        public static final int do_you_want_to_disconnect=0x7f08002f;
+        public static final int edit_note_title=0x7f080004;
+        public static final int events=0x7f080012;
+        public static final int exit=0x7f080032;
         /**  Button text [CHAR LIMIT=NONE] 
          */
-        public static final int exit_text=0x7f08002f;
-        public static final int label_search_appdata=0x7f080015;
-        public static final int label_search_query=0x7f080016;
-        public static final int location_checkbox=0x7f080018;
-        public static final int location_prompt=0x7f08001a;
-        public static final int msg_search_results=0x7f080017;
+        public static final int exit_text=0x7f080031;
+        public static final int label_search_appdata=0x7f080017;
+        public static final int label_search_query=0x7f080018;
+        public static final int location_checkbox=0x7f08001a;
+        public static final int location_prompt=0x7f08001c;
+        public static final int msg_search_results=0x7f080019;
         /**  Dialog message text [CHAR LIMIT=NONE] 
          */
-        public static final int needs_account=0x7f080028;
-        public static final int notes=0x7f08000f;
+        public static final int needs_account=0x7f08002a;
+        public static final int notes=0x7f080011;
         /**  Notification message [CHAR LIMIT=NONE] 
          */
-        public static final int registration_error=0x7f080029;
+        public static final int registration_error=0x7f08002b;
         /**  Notification message [CHAR LIMIT=NONE] 
          */
-        public static final int registration_succeeded=0x7f08002a;
-        public static final int save=0x7f08000d;
+        public static final int registration_succeeded=0x7f08002c;
+        public static final int save=0x7f08000f;
         /**  Button text [CHAR LIMIT=NONE] 
          */
-        public static final int say_hello=0x7f08002b;
-        public static final int search_hint=0x7f080019;
+        public static final int say_hello=0x7f08002d;
+        public static final int search_hint=0x7f08001b;
+        public static final int search_invoke=0x7f080005;
         public static final int search_title=0x7f080003;
         /**  List view header text [CHAR LIMIT=NONE] 
          */
-        public static final int select_account=0x7f08002c;
-        public static final int settings=0x7f080012;
+        public static final int select_account=0x7f08002e;
+        public static final int settings=0x7f080014;
         /**  Dialog button text [CHAR LIMIT=NONE] 
          */
-        public static final int skip=0x7f08002e;
-        public static final int tag_start=0x7f080013;
-        public static final int task_date=0x7f08000a;
-        public static final int today=0x7f08000e;
+        public static final int skip=0x7f080030;
+        public static final int tag_start=0x7f080015;
+        public static final int task_date=0x7f08000c;
+        public static final int today=0x7f080010;
         /**  Notification message [CHAR LIMIT=NONE] 
          */
-        public static final int unregistration_succeeded=0x7f080031;
+        public static final int unregistration_succeeded=0x7f080033;
     }
     public static final class style {
         public static final int ListItem=0x7f09000e;
